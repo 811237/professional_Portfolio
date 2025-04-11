b7bcba2acfb34770dbaa8ce2d5ae5130f49af5f3
811237 <142992805+811237@users.noreply.github.com>
2025-04-10 20:01:22 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,22 @@
+// Initiates scripts after DOM is loaded;
+$(document).ready(function() {
+
+    // Database initialization and tree housing;
+    firebase.initialize({projectName: 'CHANGEME***'});
+    var database = firebase.database();
+
+    // Global variable housing;
+
+
+
+    // Function housing;
+
+
+
+    // Run on load housing;
+
+
+
+    // Action-lead event housing;
+
+});
