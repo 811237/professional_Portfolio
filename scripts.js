d49a761bb81ab02d2a831041e3710924d08fc09e
811237 <142992805+811237@users.noreply.github.com>
2025-04-17 18:42:20 -0400

    another one

--- scripts.js
+++ scripts.js
@@ -1,10 +1,6 @@
 // Initiates scripts after DOM is loaded;
 $(document).ready(function() {
 
-    // Database initialization and tree housing;
-    firebase.initialize({projectName: 'CHANGEME***'});
-    var database = firebase.database();
-
     // Global variable housing;
 
 
