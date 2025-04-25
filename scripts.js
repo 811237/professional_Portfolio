82e8d1ff6edf4294334ce37f5c773a1dbc4fda7a
811237 <142992805+811237@users.noreply.github.com>
2025-04-25 10:35:28 -0400

    ASDFGHNJMK,.

--- scripts.js
+++ scripts.js
@@ -14,5 +14,16 @@ $(document).ready(function() {
 
 
     // Action-lead event housing;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.nav-name').css({'grid-row': '1 / 3'});
+            $('.nav-email').css({'display': 'none'});
+            $('.top-navbar').css({'height': '60px'});
+        }else {
+            $('.nav-email').css({'display': 'block'});
+            $('.nav-name').css({'grid-row': '1 / 2'});
+            $('.top-navbar').css({'height': '120px'});
+        }
+    });
 
 });
