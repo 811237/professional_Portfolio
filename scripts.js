947d31824ba1e21e75c7d53a6ede8f5530cd0f50
811237 <142992805+811237@users.noreply.github.com>
2025-05-07 08:56:18 -0400

    Revert "gfhbjkm"
    This reverts commit 35d8e6d9240abde11b5b21566f9375cecebe58be.

--- scripts.js
+++ scripts.js
@@ -16,25 +16,14 @@ $(document).ready(function() {
     // Action-lead event housing;
     $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
-            $('.nav-email').fadeOut(100);
-            $('.top-navbar').css({
-                'height': '60px',
-                'border-bottom': '2px solid lightgrey',});
-            }else {
-            $('.nav-email').fadeIn(100);
-            $('.top-navbar').css({
-                'height': '120px',
-                'border-bottom': 'none',});
+            $('.nav-name').css({'grid-row': '1 / 3'});
+            $('.nav-email').css({'display': 'none'});
+            $('.top-navbar').css({'height': '60px'});
+        }else {
+            $('.nav-email').css({'display': 'block'});
+            $('.nav-name').css({'grid-row': '1 / 2'});
+            $('.top-navbar').css({'height': '120px'});
         }
-    });
-
-    let contact_rotate = 0;
-    $('.contact').click(function() {
-        contact_rotate += 180;
-        let b = contact_rotate + 'deg';
-        $('.toggle').css({
-            'transform': `rotate(${b})`
-        })
     });
 
 });
